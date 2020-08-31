db5d9c3737b7cae1108b27af40938035c7bc8a6e
Smith, Hayden Susanna <hsmith307@gatech.edu>
2020-08-31 16:39:31 -0400

    materialUI default set up

--- src/App.js
+++ src/App.js
@@ -16,6 +16,18 @@ import Line from './containers/Line';
 import Main from './components/Main';
 import NotFound from './containers/404';
 import NavMenu from './components/NavMenu';
+import {createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#55893d"
+        },
+        secondary: {
+            main: "#88bd40"
+        }
+    }
+});
 
 function App () {
 
@@ -30,69 +42,72 @@ function App () {
 
     var arrayDat = [125, 230, 345, 260, 441, 335, 170, 294, 312, 145];
 
-    return <div>
-        <div className="App container">
-            <Navbar fluid collapseOnSelect>
-                <Navbar.Brand>
-                    <Link to="/">
-                        <img src={logo} alt="Logo" align="left"/>
-                    </Link>
-                </Navbar.Brand>
-                <Navbar.Toggle/>
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto"/>
-                    <Nav>
-                        {isAuthenticated
-                            ? <Nav.Link onClick={handleLogout}><button>Logout</button></Nav.Link>
-                            : <>
-                                <LinkContainer to="/signup">
-                                    <button>Signup</button>
-                                </LinkContainer>
-                                <LinkContainer to="/login">
-                                    <button>Login</button>
-                                </LinkContainer>
-                            </>
-                        }
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        </div>
-        <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+    return (
+    <ThemeProvider theme={theme}>
+        <div>
+            <div className="App container">
+                <Navbar fluid collapseOnSelect>
+                    <Navbar.Brand>
+                        <Link to="/">
+                            <img src={logo} alt="Logo" align="left"/>
+                        </Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle/>
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mr-auto"/>
+                        <Nav>
+                            {isAuthenticated
+                                ? <Nav.Link onClick={handleLogout}><button>Logout</button></Nav.Link>
+                                : <>
+                                    <LinkContainer to="/signup">
+                                        <button>Signup</button>
+                                    </LinkContainer>
+                                    <LinkContainer to="/login">
+                                        <button>Login</button>
+                                    </LinkContainer>
+                                </>
+                            }
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
+            <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
 
-            {/* is this the right place to put the route */}
+                {/* is this the right place to put the route */}
 
-            <Router>
+                <Router>
+                    <Switch>
+                    {/* <Route path="/login" component={Login}/> */}
+                        <Route path="/main" component={Main} exact></Route>
+                        <Route path="/" component={NavMenu}></Route>
+                        <Route component={NotFound} />
+                    </Switch>
+                </Router>
+                {/* <Routes /> */}
+            </AppContext.Provider>
+
+            {/* <Chart></Chart>
+            <div class="linechart">
+                <svg
+                className="lineChartSvg"
+                >
+                <g>
+                    <XAxis></XAxis>
+                    <YAxis></YAxis>
+                    <Line/>
+                </g>
+                </svg>
+            </div> */}
+
+            {/* <Router>
                 <Switch>
-                {/* <Route path="/login" component={Login}/> */}
+                {/* <Route path="/login" component={Login}/>
                     <Route path="/main" component={Main} exact></Route>
-                    <Route path="/" component={NavMenu}></Route>
                     <Route component={NotFound} />
                 </Switch>
-            </Router>
-            {/* <Routes /> */}
-        </AppContext.Provider>
-
-        {/* <Chart></Chart>
-        <div class="linechart">
-            <svg
-            className="lineChartSvg"
-            >
-            <g>
-                <XAxis></XAxis>
-                <YAxis></YAxis>
-                <Line/>
-            </g>
-            </svg>
-        </div> */}
-
-        {/* <Router>
-            <Switch>
-            {/* <Route path="/login" component={Login}/>
-                <Route path="/main" component={Main} exact></Route>
-                <Route component={NotFound} />
-            </Switch>
-        </Router> */}
-    </div>
-
+            </Router> */}
+        </div>
+    </ThemeProvider>
+    )
 }
 export default App;
