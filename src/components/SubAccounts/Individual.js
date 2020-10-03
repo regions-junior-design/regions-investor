9754cc6b82c2bc9ea931e395a988151213c1f856
Smith, Hayden Susanna <hsmith307@gatech.edu>
2020-10-03 13:20:48 -0400

    prepping

--- src/components/SubAccounts/Individual.js
+++ src/components/SubAccounts/Individual.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default function Individual(props) {
     return(
         <React.Fragment>
-            
+            <Typography variant='h1'>
+                Subaccount Name: 
+            </Typography>
         </React.Fragment>
     )
 }
